93c0ed6b84507dbb13c5137e015de0f0be874695
Thomas Maximini <tmaximini@gmail.com>
2023-01-29 09:16:26 +0100

    favico

--- components/Container.tsx
+++ components/Container.tsx
@@ -48,6 +48,12 @@ export default function Container(props) {
     <div className="bg-gray-50 dark:bg-gray-900 min-h-[100vh]">
       <Head>
         <title>{meta.title}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
         <meta content={meta.keywords} name="keywords" />
