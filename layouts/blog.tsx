8442721488b76874dc4c7c653ac63e3ef3c8f841
Thomas Maximini <tmaximini@gmail.com>
2023-02-02 09:40:58 +0100

    image

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -1,24 +1,20 @@
 import Image from 'next/image';
 import { parseISO, format } from 'date-fns';
 
-
 import type { PropsWithChildren } from 'react';
 import type { Blog } from '../types';
 import Container from '../components/Container';
 
-
-export default function BlogLayout({
-  children,
-  post
-}: PropsWithChildren<{ post: Blog }>) {
+export default function BlogLayout({ children, post }: PropsWithChildren<{ post: Blog }>) {
   return (
     <Container
       title={`${post.title} – Thomas Maximini`}
       description={post.summary}
       keywords={post.keywords}
-      image={`https://maxi.io${post.image}`}
+      image={post.image ? `https://maxi.io${post.image}` : 'https://maxi.io/avatar.png'}
       date={new Date(post.publishedAt).toISOString()}
-      type="article">
+      type="article"
+    >
       <article className="flex flex-col items-start justify-center w-full max-w-3xl mx-auto mb-16">
         <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
           {post.title}
@@ -41,13 +37,8 @@ export default function BlogLayout({
             {post.readingTime.text}
           </p>
         </div>
-        <div className="w-full mt-4 prose dark:prose-dark max-w-none">
-          {children}
-        </div>
-
-
+        <div className="w-full mt-4 prose dark:prose-dark max-w-none">{children}</div>
       </article>
     </Container>
-
   );
 }
