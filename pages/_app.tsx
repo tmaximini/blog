ea4eda0c319a9466ebb56736e8b9d8d67d14b370
Thomas Maximini <tmaximini@gmail.com>
2023-02-05 17:26:41 +0100

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ThemeProvider attribute="class">
-    <Component {...pageProps} />
+    <ThemeProvider attribute="class">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2BF0B7GYMQ"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2BF0B7GYMQ');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
